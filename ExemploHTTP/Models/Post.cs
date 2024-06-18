3b85412ee6b53d1a4cb47030efec459d1b61449b
Emilly <millysantoss1106@gmail.com>
2024-06-18 10:45:47 -0300

    Exemplo HTTP

---
+++ ExemploHTTP/Models/Post.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploHTTP.Models
+{
+    public class Post
+    {
+
+        //Botão direito, Quick Actions ... /  
+        public int UserID { get; set; }
+
+        private int id;
+        public int Id { get => Id; set => Id = value; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+
+
+
+
+        // userId
+        // Id
+        // Title
+        //Body
+    }
+}
---
+++ ExemploHTTP/Services/RestService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using ExemploHTTP.Models;
+using System.Text.Json;
+using System.Diagnostics;
+
+namespace ExemploHTTP.Services
+{
+    public class RestService
+    {
+        private HttpClient client;
+        private Post post;
+        private List<Post> posts;
+        private JsonSerializerOptions _serializeOptions;
+
+        RestService()
+        {
+            client = new HttpClient();
+            _serializeOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true,
+            };
+            
+
+             
+        
+        }
+
+        public async Task<List<Post>> getPostsAsync()
+        {
+            Uri uri = new Uri("https://jsonplaceholder.typicode.com/posts");
+            try
+            {
+
+
+
+
+                HttpResponseMessage response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+
+                    string content = await response.Content.ReadAsStringAsync();
+                    posts = JsonSerializer.Deserialize<List<Post>>(content, _serializeOptions);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            return posts;
+
+        }
+
+    }
+}
